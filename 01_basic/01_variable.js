c559bc22454e98aaf53c909cdd16f17c417b6745
soumya720 <57815662+soumya720@users.noreply.github.com>
2025-01-03 11:36:59 +0000

    code changes 4 files

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,26 @@
+const accountId = 144553
+let accountEmail = "hitesg@google.com"
+var accountPassword = "12345"
+accountCity = "Jajpur"
+
+//accountId = 2 // not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+//boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+//console.log(typeof null); //object
+//console.log(typeof undefined); //undefined
+
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,76 @@
+// let score = "abc"
+// console.log(typeof score);  //string
+// console.log(typeof(score));  //string
+// let valueInNumber = Number(score)  //number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);  //NaN
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+// let isLoggedIn = "hitesh"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);  //true
+
+// 1=> true; 0 => false
+//"" => false
+//"hitesh" => true
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber); //33
+// console.log(typeof stringNumber); //string
+ 
+//************************** Operations ***********************/
+// let value = 3
+// let negValue = -value
+// console.log(negValue);   //-3
+// console.log(2+2);      //4
+// console.log(2-2);     //0
+// console.log(2*2);   //4
+// console.log(2**3); //8
+// console.log(2/3); //0.66
+// console.log(2%3); //2
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+// let str3 = str1 + str2
+// console.log(str3); // hello hitesh
+
+// console.log("1" + 2);//12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //12
+// console.log(1 + 2 + "2"); //122
+
+// console.log((3 + 4) * 5 % 3); //32
+// console.log(true);  //true
+
+// console.log(+true); //1
+// console.log(+"");   //0
+// console.log(-true); //-1
+// console.log(false); // false
+
+//Postfix Increement
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+//Prefix Increment=>
+
+//     let x = 3;
+// const y = ++x;
+// x is 4; y is 4
+
+// let x2 = 3n;
+// const y2 = ++x2;
+// x2 is 4n; y2 is 4n
