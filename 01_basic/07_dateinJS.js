0c7797bd27ecb5946d5ac28ed16d8d5043a2535b
soumya720 <57815662+soumya720@users.noreply.github.com>
2025-01-04 03:51:51 +0000

    date

---
+++ 01_basic/07_dateinJS.js
@@ -0,0 +1,24 @@
+//Date
+//let myDate = new Date()
+//console.log(myDate.toString());  //Sat Jan 04 2025 01:23:20 GMT+0000 (Coordinated Universal Time)
+//console.log(myDate.toDateString()); //Sat Jan 04 2025
+//console.log(myDate.toLocaleString()); //1/4/2025, 1:23:20 AM
+//console.log(typeof myDate);   //object
+
+//let myCreatedDate = new Date(2023, 0, 23)
+//let myCreatedDate = new Date(2025, 0, 4)
+//let myCreatedDate = new Date(2025, 0, 23, 7, 18)
+//let myCreatedDate = new Date("01-04-2025")
+//console.log(myCreatedDate.toLocaleString());
+//let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+//console.log(Date.now());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+//'${newDate.getDay()} and the time'
+newDate.toLocaleString('default',{weekday: "long"})
