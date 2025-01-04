fbd6e60653bac0b8f5fa38a94eb215e2e7f9f5f1
soumya720 <57815662+soumya720@users.noreply.github.com>
2025-01-04 01:13:47 +0000

    nums and math

---
+++ 01_basic/05_string.js
@@ -0,0 +1,22 @@
+//const name = "hitesh"
+//const repocount = 50
+// console.log(name + repocount + " Value");
+////console.log(`Hello my name is ${name} and repo count is ${repoCount}`);
+// const gameName = new String('hiteshhc')
+// console.log(gameName[0]);
+// console.log(gameName._proto_);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('2'))
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+// const anotherString = gameName.slice(-7,4)
+// console.log(anotherString);
+// const newStringOne = " hitesh  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+// const url = "https://hitesh.com/hitesh%20choudary"
+// console.log(url.replace('%20'), '_')
+// console.log(url.includes('.com'))
---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,41 @@
+// Primitive
+// 7 types : Strings,Number, Boolean, null, undefined, Symbol, Bigint
+
+// const score = 100
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+// const id = Symbol('123')
+// const anotherId =Symbol('123')
+// console.log(id === anotherId);
+// const bigNumber = 3456543576654356754n
+
+//Referance (Non Primitive)
+//Array, Objects, Functions
+
+// const heros = ["shaktiman","naagraj","doga"];
+// let myObj = {name: "hitesh",
+//     age: 22,
+// }
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack (Primitive), heap (Non-Primitive)
+//let myYoutubename = "soumyapatra.com"
+// anothername = "chaiaurcode"
+// console.log(myYoutubename);
+// console.log(anothername);
+// let userOne = {
+//     email: "user@google.com",
+//     upi: "user@ybl"
+// }
+
+// let userTwo = userOne
+// userTwo.email = "hitesh@google.com"
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
