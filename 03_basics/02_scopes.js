84a4efe6f536dd65ef306ea2a473badb39399633
soumya720 <57815662+soumya720@users.noreply.github.com>
2025-01-05 04:42:19 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,58 @@
+if (true){
+    let a =10
+    const b = 20
+    console.log("INNER: " ,a);
+    }
+
+for (let i = 0; i<Array.length; i++)
+{
+const element = array[i];
+}
+const b = 20
+//var c = 300
+console.log(a);
+console.log(b);
+console.log(c);
+function one(){
+    const username = "hitesh"
+}
+
+function two(){
+    const website = "youtube"
+    console.log(username);
+}
+//console.log(website);
+two()
+
+//one()
+if (true){
+    const username = "hitesh"
+    if (username === "hitesh"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(username);
+}
+// console.log(username)
+
+if (true){
+    const username = "hitesh"
+    if (username === "hitesh"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+   // console.log(website);
+}
+//console.log(username);
+
+//+++++++++++++++++++++++++ intresting ++++++++++++++
+
+function addone(num){
+    return num+1
+}
+addone(5)
+
+const addTwo = function(num){
+    return num+2
+}
+addone(5)
