ce5df8e307bd93187a4f6544a365e1ca842cfdf8
soumya720 <57815662+soumya720@users.noreply.github.com>
2025-01-05 12:53:36 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,23 @@
+// Immediately Invoked Function Expression (IIFE)
+
+// (function chai(){
+//     console.log(`DB CONNECTED`);
+// })();
+
+// (()=>{
+//     console.log(`DB CONNECTED TWO ${name}`);
+// })('hitesh');
+
+// function one
+// (){
+//     console.log("o0ne");
+// }
+// function two(){
+//     console.log("two")
+// }
+// function three(){
+//     console.log("three");
+// }
+// one()
+// two()
+// three()
